9881a4bd0a3064e8e6d07c8f26dcf2700a00efcb
Amenonuhoko <mauriza.arianne@gmail.com>
2021-01-17 16:43:23 +1100

    multiple-cards

--- scripts/index.js
+++ scripts/index.js
@@ -1,21 +1,23 @@
-const forms = document.getElementById("manga-form").elements;
-const submitButton = forms["submit"];
+// const image = document.getElementById("image");
+// const header = document.getElementById("header");
+// const synopsis = document.getElementById("synopsis");
+const container = document.querySelector(".page-container");
 
-const image = document.getElementById("image");
-const header = document.getElementById("header");
-const synopsis = document.getElementById("synopsis");
-
+// バカやろ
 const getManga = async () => {
-	// Move for other types
+	// Subject to other type
 	const url = "https://kitsu.io/api/edge";
 	//For this type
 	const type = "/manga";
-
+	// Form declaration
+	const forms = document.getElementById("manga-form").elements;
+	const submitButton = forms["submit"];
 	const typeQuery = forms["search-param"].value;
 	const wordQuery = forms["input"].value;
 	// Combined endpoint
 	const endpoint = `${url}${type}?filter%5B${typeQuery}%5D=${wordQuery}`;
 	console.log(endpoint);
+	// Fetch
 	let response = await fetch(endpoint, {
 		Accept: "application/vnd.api+json",
 		"Content-Type": "application/vnd.api+json",
@@ -30,15 +32,36 @@ const getManga = async () => {
 	}
 };
 
-const renderObj = async () => {
-	const obj = await getManga();
-	console.log(obj);
-	image.src = obj.data["0"].attributes.posterImage.small;
-	header.innerHTML = obj.data["0"].attributes.titles.en;
-	synopsis.innerHTML = obj.data["0"].attributes.synopsis;
+const createDocument = async () => {
+	// Obj
+	const objArr = await getManga();
+	// Iterate
+	objArr.data.forEach((arr, index) => {
+		// Declare
+		const populateImage = arr.attributes.posterImage.small;
+		const populateTitle = arr.attributes.titles.en;
+		const populateSyn = arr.attributes.synopsis;
+		// Template
+		const newImage = `<img class="image" src=${populateImage}>`;
+		const newTitle = `<h1 class="header">${populateTitle}</h1>`;
+		const newSyn = `<p class="synopsis">${populateSyn}</p>`;
+		const newTemplate = `
+			<div class="manga-cards" id="manga-card-${index}">
+			${newImage}
+				<div class="detail">
+					${newTitle}
+					${newSyn}
+				</div>
+			</div>
+			`;
+		// Add
+		container.insertAdjacentHTML("beforeend", newTemplate);
+	});
 };
 
-submitButton.addEventListener("click", function (event) {
-	event.preventDefault();
-	renderObj();
+const form = document.getElementById("manga-form");
+form.addEventListener("submit", (e) => {
+	console.log(e);
+	e.preventDefault();
+	createDocument();
 });
